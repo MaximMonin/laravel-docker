58c1f0316473d4273403c1fca695a54b90fbf5d7
maximmonin <maximmonin@gmail.com>
2020-05-24 00:38:27 +0300

    Added Auth+Localization to docker image

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 docker rm -f laravel
-docker run -d -v /tmp:/tmp --name laravel laravel
-sleep 5
+docker run -d -v $(pwd)/tmp:/tmp --name laravel laravel
 docker exec -i laravel cp -R /app /tmp
 docker rm -f laravel
-mv /tmp/app .
+mv $(pwd)/tmp/app .
+chmod -R a+rw app
+rm -r tmp
 cp env-example ./app/.env
-sleep 5
 docker run -d -v $(pwd)/app:/app --name laravel laravel
-sleep 5
 docker exec -i laravel chown -R www-data:www-data /app/bootstrap /app/storage
 docker rm -f laravel
